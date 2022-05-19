a5117324e315bb78151e511ac48184fb348b3c37
Анна Шаповалова <aaaananas1@gmail.com>
2022-05-19 17:56:34 +0300

    animation for mobile menu icons was added

--- src/js/app.js
+++ src/js/app.js
@@ -14,30 +14,36 @@ $('.banner').slick({
 	slidesToShow: 1,
 	arrows: false,
 });
- 
+
 //mobile menu tabs
-$('.js-tab-trigger').click(function() {
-   var id = $(this).attr('data-tab'),
-       content = $('.js-tab-content[data-tab="'+ id +'"]');
-   
-   $('.js-tab-trigger.active').removeClass('active');
-   $(this).addClass('active');
-   
-   $('.js-tab-content.active').removeClass('active');
-   content.addClass('active');
+$('.js-tab-trigger').click(function () {
+	var id = $(this).attr('data-tab'),
+		content = $('.js-tab-content[data-tab="' + id + '"]');
+
+	$('.js-tab-trigger.active').removeClass('active');
+	$(this).addClass('active');
+
+	$('.js-tab-content.active').removeClass('active');
+	content.addClass('active');
 });
 
 //mobile menu accordeon
 var acc = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) 
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
+for (i = 0; i < acc.length; i++)
+	acc[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
+
+// $(document).ready(function () {
+// 	$(".accordion").click(function (event) {
+// 		$(".accordion-icon").toggleClass("active");
+// 	});
+// });
