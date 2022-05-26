0e3d5e0f59448aef47608419221f40ea7bde62fc
Анна Шаповалова <aaaananas1@gmail.com>
2022-05-26 18:12:12 +0300

    block popular is over

--- src/js/app.js
+++ src/js/app.js
@@ -55,7 +55,6 @@ $('.popular-slider').slick({
 	speed: 300,
 	slidesToShow: 2,
 	slidesToScroll: 1,
-	variableWidth: true,
 	responsive: [
 		{
 			breakpoint: 768,
@@ -69,8 +68,5 @@ $('.popular-slider').slick({
 				slidesToShow: 4,
 			}
 		},
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
 	]
 });
