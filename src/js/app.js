434a7665eeda1b9e6fad0e878a452e6c809939d9
Анна Шаповалова <aaaananas1@gmail.com>
2022-05-30 17:42:42 +0300

    all women goods menu is added

--- src/js/app.js
+++ src/js/app.js
@@ -30,7 +30,6 @@ $('.js-tab-trigger').click(function () {
 //mobile menu accordeon
 var acc = document.getElementsByClassName("accordion");
 var i;
-
 for (i = 0; i < acc.length; i++)
 	acc[i].addEventListener("click", function () {
 		this.classList.toggle("active");
